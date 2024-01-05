e5cd91bac139cd31149c3aebce9af4cf63cac6f4
abduganii <abduganiabdu2@gmail.com>
2024-01-05 15:28:12 +0500

    fixed

--- src/components/UI/header/index.jsx
+++ src/components/UI/header/index.jsx
@@ -1,11 +1,31 @@
+"use client";
 import Image from "next/image";
 import { Navbar } from "./data";
 import cls from "./header.module.scss";
 import Lang from "../lang";
+import { useEffect, useState } from "react";
 
 export default function Header({ lang }) {
+  const [windowHeight, setWindowHeight] = useState(
+    typeof window !== "undefined" ? window.scrollY : 0
+  );
+
+  const getWindowHeight = () => {
+    setWindowHeight(typeof window !== "undefined" ? window.scrollY : 0);
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", getWindowHeight);
+      return () => window.removeEventListener("scroll", getWindowHeight);
+    }
+  }, []);
   return (
-    <header className="py-4 z-10 fixed w-full bg-white">
+    <header
+      className={`${
+        windowHeight > 0 && "shadow-xl"
+      } fixed py-4 z-10  w-full bg-white top-0 left-0 w-full `}
+    >
       <div className="container flex  justify-between items-center	">
         <div className={cls.burger__btn}>
           <Image
